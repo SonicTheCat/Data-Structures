4b8c9abd886a763026f1de92bff35f49c9fa6d8d
SonicTheCat <paveliliev90@gmail.com>
2019-01-24 04:50:18 +0200

    ROPE AND TRIE EXERCISE

---
+++ 16.Rope And Trie - Exercise/TextEditor/StartUp.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        // 71/100
+
+        TextEditor editor = new TextEditor();
+
+        string input;
+        while ((input = Console.ReadLine()) != "end")
+        {
+            var tokens = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var command = tokens[0];
+
+            if (command == "login")
+            {
+                editor.Login(tokens[1]);
+            }
+            else if (command == "logout")
+            {
+                editor.Logout(tokens[1]);
+            }
+            else if (command == "users")
+            {
+                var prefix = tokens.Length == 2 ? tokens[2] : "";
+                var users = editor.Users(prefix);
+
+                foreach (var user in users)
+                {
+                    Print(user);
+                }
+            }
+            else
+            {
+                OtherCommands(editor, tokens);
+            }
+        }
+    }
+
+    public static void OtherCommands(TextEditor editor, string[] tokens)
+    {
+        var username = tokens[0];
+        var command = tokens[1];
+
+        switch (command)
+        {
+            case "insert":
+                {
+                    var index = int.Parse(tokens[2]);
+                    var text = string
+                        .Join(" ", tokens
+                        .Skip(3)
+                        .ToArray())
+                        .Trim('"');
+
+                    editor.Insert(username, index, text);
+                }
+                break;
+            case "prepend":
+                {
+                    var text = tokens[2].Trim('"');
+                    editor.Prepend(username, text);
+                }
+                break;
+            case "substring":
+                {
+                    var startIndex = int.Parse(tokens[2]);
+                    var lenght = int.Parse(tokens[3]);
+                    editor.Substring(username, startIndex, lenght);
+                }
+                break;
+            case "delete":
+                {
+                    var startIndex = int.Parse(tokens[2]);
+                    var lenght = int.Parse(tokens[3]);
+                    editor.Delete(username, startIndex, lenght);
+                }
+                break;
+            case "clear":
+                {
+                    editor.Clear(username);
+                }
+                break;
+            case "length":
+                {
+                    var result = editor.Length(username);
+                    Print(result.ToString());
+                }
+                break;
+            case "print":
+                {
+                    var result = editor.Print(username);
+                    if (result != null)
+                    {
+                        Print(result);
+                    }
+                }
+                break;
+            case "undo":
+                {
+                    editor.Undo(username);
+                }
+                break;
+        }
+    }
+
+    public static void Print(string result)
+    {
+        Console.WriteLine(result);
+    }
+}
---
+++ 16.Rope And Trie - Exercise/TextEditor/TextEditor.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections.Generic;
+using Wintellect.PowerCollections;
+
+public class TextEditor : ITextEditor
+{
+    private readonly Trie<BigList<char>> usersStrings;
+    private readonly Trie<Stack<string>> oldStrings;
+    private readonly HashSet<string> allUsers;
+
+    public TextEditor()
+    {
+        this.usersStrings = new Trie<BigList<char>>();
+        this.oldStrings = new Trie<Stack<string>>();
+        this.allUsers = new HashSet<string>();
+    }
+
+    public void Clear(string username)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return;
+        }
+
+        var bigList = this.usersStrings.GetValue(username);
+
+        this.PutOldStringInStack(bigList, username);
+
+        bigList.Clear();
+    }
+
+    public void Delete(string username, int startIndex, int length)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return;
+        }
+
+        var bigList = this.usersStrings.GetValue(username);
+
+        this.PutOldStringInStack(bigList, username);
+
+        bigList.RemoveRange(startIndex, length);
+    }
+
+    public void Insert(string username, int index, string str)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return;
+        }
+
+        var bigList = this.usersStrings.GetValue(username);
+
+        var oldString = string.Join("", bigList);
+        if (oldString.Length >= index)
+        {
+            this.PutOldStringInStack(bigList, username);
+
+            bigList.InsertRange(index, str);
+        }
+    }
+
+    public int Length(string username)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return 0;
+        }
+
+        return this.usersStrings.GetValue(username).Count;
+    }
+
+    public void Login(string username)
+    {
+        this.usersStrings.Insert(username, new BigList<char>());
+        this.oldStrings.Insert(username, new Stack<string>());
+        this.allUsers.Add(username);
+    }
+
+    public void Logout(string username)
+    {
+        this.usersStrings.Delete(username);
+        this.oldStrings.Delete(username);
+        this.allUsers.Remove(username);
+    }
+
+    public void Prepend(string username, string str)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return;
+        }
+
+        var bigList = this.usersStrings.GetValue(username);
+
+        this.PutOldStringInStack(bigList, username);
+
+        bigList.InsertRange(0, str);
+    }
+
+    public string Print(string username)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return null;
+        }
+
+        var value = this.usersStrings.GetValue(username);
+
+        return string.Join("", value);
+    }
+
+    public void Substring(string username, int startIndex, int length)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return;
+        }
+
+        var bigList = this.usersStrings.GetValue(username);
+
+        this.PutOldStringInStack(bigList, username);
+
+        var subStr = bigList.GetRange(startIndex, length);
+        bigList.Clear();
+        bigList.AddRange(subStr);
+    }
+
+    public void Undo(string username)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return;
+        }
+
+        var stack = this.oldStrings.GetValue(username);
+        if (stack.Count == 0)
+        {
+            return;
+        }
+
+        var oldString = stack.Pop();
+
+        var bigList = this.usersStrings.GetValue(username);
+
+        bigList.Clear();
+        bigList.AddRange(oldString);
+    }
+
+    public IEnumerable<string> Users(string prefix = "")
+    {
+        if (prefix != string.Empty)
+        {
+            foreach (var user in this.usersStrings.GetByPrefix(prefix))
+            {
+                yield return user;
+            }
+        }
+        else
+        {
+            foreach (var user in this.allUsers)
+            {
+                yield return user;
+            }
+        }
+    }
+
+    private void PutOldStringInStack(BigList<char> bigList, string username)
+    {
+        var oldString = string.Join("", bigList);
+
+        this.oldStrings.GetValue(username).Push(oldString);
+    }
+}
